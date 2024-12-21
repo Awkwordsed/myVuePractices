e0b394c0be3de1c65f2b90f743e280312716b67d
sed <119004237+Awkwordsed@users.noreply.github.com>
2024-12-21 19:03:41 +0330

    Add files via upload

---
+++ component/infiniteLoad.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h3>{{ currentCharacter.name }}</h3>
+    <p>loading...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      characters: [
+        { name: '△' },
+        { name: '▷' },
+        { name: '▽' },
+        { name: '◁' },
+      ],
+      currentIndex: 0,
+    };
+  },
+  computed: {
+    currentCharacter() {
+      return this.characters[this.currentIndex];
+    },
+  },
+  mounted() {
+    this.startAutoChange();
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    startAutoChange() {
+      this.interval = setInterval(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.characters.length;
+      }, 200); // Change character every 200 milliseconds (0.2 second)
+    },
+  },
+};
+</script>
